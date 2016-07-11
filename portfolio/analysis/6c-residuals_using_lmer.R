9f36285cf51c0751ce1b9b18bd7a88b63843e2e2
Sean Anderson <sean@seananderson.ca>
2016-07-11 11:07:20 -0700

    Strip trailing white space

--- portfolio/analysis/6c-residuals_using_lmer.R
+++ portfolio/analysis/6c-residuals_using_lmer.R
@@ -1,10 +1,10 @@
-# Experiment with models 
+# Experiment with models
 # by fitting them quickly with TMB
 
 # 2 separate but more complicated models. More interactions:
 # - lmer() model to full dataset: interactions allowed to vary?
 # - (1) model for mean, (2) model for variance (log sd)
-# random effects in strategy? 
+# random effects in strategy?
 # days as a covariate?
 # 1. What's benefit for individual person
 # 2. Benefit across people within a strategy [does this change with div]
@@ -31,7 +31,7 @@ cfecAnnual.diff = cfecAnnual.diff[which(cfecAnnual.diff$strategy == cfecAnnual.d
 # also filter out people making < 1000 / year
 cfecAnnual.diff = cfecAnnual.diff[which(cfecAnnual.diff$revenue >= 5000), ]
 
-# 4200 different strategies, need to model only most common, 
+# 4200 different strategies, need to model only most common,
 top.strategies = names(rev(sort(table(cfecAnnual.diff$strategy)))[1:100])
 cfecAnnual.diff = cfecAnnual.diff[cfecAnnual.diff$strategy%in%top.strategies, ]
 testData = cfecAnnual.diff
@@ -45,24 +45,24 @@ testData$rev.pctChange = qnorm(pt(testData$rev.pctChange, df = fit.t$estimate[3]
 # random slope, fixed intercept at 0 because log-diff data has mean 0
 # boxplot(testData$rev.pctChange ~ testData$strategy, outline=F)
 #lm1 = lm(rev.pctChange ~ -1 + specDiv.pctChange + (specDiv.pctChange:strategy+days.pctChange:strategy), data = testData)
-lm2 = lmer(rev.pctChange ~ -1 + (-1+specDiv.pctChange+days.pctChange|strategy) + 
-    specDiv.pctChange + days.pctChange, data = testData) 
+lm2 = lmer(rev.pctChange ~ -1 + (-1+specDiv.pctChange+days.pctChange|strategy) +
+    specDiv.pctChange + days.pctChange, data = testData)
 r.squaredGLMM(lm2)
 
 testData$residuals <- residuals(lm2)
 
 # plot fitted vs observed -- non salmon specialists
 non.sal= which(substr(testData$strategy,1,1)!="S")
-ggplot(testData[non.sal,], aes(x=fitted.values(lm2)[non.sal], y=rev.pctChange, color = strategy)) + 
-  geom_point(alpha = 0.7) + 
-  theme(aspect.ratio=1) + 
+ggplot(testData[non.sal,], aes(x=fitted.values(lm2)[non.sal], y=rev.pctChange, color = strategy)) +
+  geom_point(alpha = 0.7) +
+  theme(aspect.ratio=1) +
   geom_abline(slope=1, intercept=0,color = "grey50")
 ggsave("residuals/fitted_v_observed_nonSalmon.pdf", width = 40, height = 40, units = "cm")
 
 # plot fitted vs observed
-ggplot(testData, aes(x=fitted.values(lm2), y=rev.pctChange, color = log(revenue.prev))) + 
+ggplot(testData, aes(x=fitted.values(lm2), y=rev.pctChange, color = log(revenue.prev))) +
   facet_wrap(~strategy, scale="free") +
-  geom_point(alpha = 0.7) + 
+  geom_point(alpha = 0.7) +
   geom_abline(slope=1, intercept=0, color = "grey50")
 ggsave("residuals/fitted_v_observed.pdf", width = 40, height = 40, units = "cm")
 
@@ -118,20 +118,20 @@ ggsave("residuals/year_v_sqrtabsresid.pdf", width = 40, height = 40, units = "cm
 
 # Another model validation = prediction
 # We can also do some diagnostics and hold out ~ 3 people per strategy, then predict their response
-# 
-testData$row = seq(1, dim(testData)[1]) # id 
-#testData.test = group_by(testData, strategy) %>% 
+#
+testData$row = seq(1, dim(testData)[1]) # id
+#testData.test = group_by(testData, strategy) %>%
 #  sample_n(3)
 test_set = sample(seq(1, dim(testData)[1]), size=20000, replace=F)
 testData.test = testData[test_set,]
 testData.train = testData[-test_set,] # training set
-lm.train = lmer(rev.pctChange ~ -1 + (-1+specDiv.pctChange+days.pctChange|strategy) + 
-    specDiv.pctChange + days.pctChange, data = testData.train) 
+lm.train = lmer(rev.pctChange ~ -1 + (-1+specDiv.pctChange+days.pctChange|strategy) +
+    specDiv.pctChange + days.pctChange, data = testData.train)
 testData.test$pred = predict(lm.train, testData.test)
 
-ggplot(testData.test, aes(x=pred, y=rev.pctChange, color = log(revenue.prev))) + 
+ggplot(testData.test, aes(x=pred, y=rev.pctChange, color = log(revenue.prev))) +
   facet_wrap(~strategy, scale="free") +
-  geom_point(alpha = 0.7) + 
+  geom_point(alpha = 0.7) +
   geom_abline(slope=1, intercept=0, color = "grey50")
 ggsave("residuals/fitted_v_observed_holdout.pdf", width = 40, height = 40, units = "cm")
 
@@ -139,28 +139,28 @@ ggsave("residuals/fitted_v_observed_holdout.pdf", width = 40, height = 40, units
 ## THINGS I'M SEEING HERE
 # 1. Many of the strategies have no variance w/respect to the predictors. Specifically, permits that are
 #   targeting 1 spp (herring, halibut, sablefish) are essentially ~ 0. We can't do a good job of estimating
-#   random effects on slopes for these strategies, and probably need to just set them to 0. 
+#   random effects on slopes for these strategies, and probably need to just set them to 0.
 #
 # 2. For the differenced model, inclusion of previous revenue may be important as a covariate -- some strategies
-#   have negative relationships, contrary to the "rich get richer" idea. 
+#   have negative relationships, contrary to the "rich get richer" idea.
 #
-# 3. The distribution of the log differenced revenue isn't close to normal -- it's a t distribution with 
+# 3. The distribution of the log differenced revenue isn't close to normal -- it's a t distribution with
 #   df ~ 2.4, so changing to an alternate transformation / distribution would help some of the residual patterns
 #   fit.t = fitdistr(testData$rev.pctChange, "t")
-# 
-# 4. I found that many of the extreme events occurred for people making very little money. When we 
+#
+# 4. I found that many of the extreme events occurred for people making very little money. When we
 #   restrict the analysis to people making> 1000 or more (or any floor) the distributions are much more normal
 
 # Can alternate distribution help w/residuals? What this shows is that reducing the extreme tails worsens
-# the model R^2 a bit, but improves the QQ plot and the scale - location plot (eliminating the U or V shape) 
+# the model R^2 a bit, but improves the QQ plot and the scale - location plot (eliminating the U or V shape)
 # As an example of the residuals w/permit S15B
-lm.norm = lm(rev.pctChange ~ -1 + specDiv.pctChange+days.pctChange + log(revenue.prev), 
-  data = testData[which(testData$strategy=="S15B"),]) 
-# change to standard normal based on cdf / percentiles. 
+lm.norm = lm(rev.pctChange ~ -1 + specDiv.pctChange+days.pctChange + log(revenue.prev),
+  data = testData[which(testData$strategy=="S15B"),])
+# change to standard normal based on cdf / percentiles.
 fit.t = fitdistr(testData$rev.pctChange, "t")
 testData$rev.pctChange.transform = qnorm(pt(testData$rev.pctChange, df = fit.t$estimate[3]), 0, 1)
-lm.norm2 = lm(rev.pctChange.transform ~ as.factor(year) + specDiv.pctChange+days.pctChange + log(revenue.prev), 
-  data = testData[which(testData$strategy=="S15B"),]) 
+lm.norm2 = lm(rev.pctChange.transform ~ as.factor(year) + specDiv.pctChange+days.pctChange + log(revenue.prev),
+  data = testData[which(testData$strategy=="S15B"),])
 
 #####################################################################################
 # UNTRANSFORMED DATA. With threshold of 5 yrs / person, about the same size as
@@ -196,13 +196,13 @@ dmv <- dm %>% group_by(p_holder) %>%
 dmv$strategy <- as.factor(dmv$strategy)
 
 # This model was the original -- linear only
-m4 <- lmer(log(cv) ~ (scaled_log_spec + scaled_log_days)^2 + 
-    (1 + (scaled_log_spec + scaled_log_days)^2|strategy), 
+m4 <- lmer(log(cv) ~ (scaled_log_spec + scaled_log_days)^2 +
+    (1 + (scaled_log_spec + scaled_log_days)^2|strategy),
   data = dmv)
 
 # This model includes linear + quadratic random effects
-m6 <- lmer(log(cv) ~ scaled_log_spec * scaled_log_days + I(scaled_log_spec^2) + I(scaled_log_days^2) + 
-    (1 + scaled_log_spec:scaled_log_days + scaled_log_spec + scaled_log_days + I(scaled_log_spec^2) + I(scaled_log_days^2)|strategy), 
+m6 <- lmer(log(cv) ~ scaled_log_spec * scaled_log_days + I(scaled_log_spec^2) + I(scaled_log_days^2) +
+    (1 + scaled_log_spec:scaled_log_days + scaled_log_spec + scaled_log_days + I(scaled_log_spec^2) + I(scaled_log_days^2)|strategy),
   data = dmv)
 broom::tidy(m6, conf.int = TRUE) %>%
   filter(term != "(Intercept)") %>%
@@ -226,9 +226,9 @@ r.squaredGLMM(m5)
 dm$residuals = residuals(m5)
 
 # plot fitted vs observed
-ggplot(dm, aes(x=fitted.values(m5), y=log(revenue), col = log(revenue))) + 
+ggplot(dm, aes(x=fitted.values(m5), y=log(revenue), col = log(revenue))) +
   facet_wrap(~strategy, scale="free") +
-  geom_point(alpha = 0.7) + 
+  geom_point(alpha = 0.7) +
   geom_abline(slope=1, intercept=0, color = "grey50")
 ggsave("residuals_rev/fitted_v_observed.pdf", width = 40, height = 40, units = "cm")
 
