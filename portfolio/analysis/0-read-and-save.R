af7c81860c38397e0b9587c7f675cb0b68261858
eric-ward <eric.ward@noaa.gov>
2016-08-03 13:12:34 -0700

    directory change

--- portfolio/analysis/0-read-and-save.R
+++ portfolio/analysis/0-read-and-save.R
@@ -1,14 +1,14 @@
 # Read in, cleanup, and write out a version of the data
 
 library(dplyr)
-load("../../data/cfec_070616")
+load("data/cfec_070616")
 names(cfec) <- tolower(names(cfec))
 cfec <- as_data_frame(cfec)
 names(cfec) <- gsub("\\.", "_", names(cfec))
 cfec$year <- as.numeric(cfec$year)
 cfec$specn_orig <- as.numeric(cfec$specn_orig)
 
-deflationTable <- read.csv("../../data/deflation.csv")
+deflationTable <- read.csv("data/deflation.csv")
 
 # Adjust price and g_earn for inflation
 cfec$year <- as.numeric(cfec$year)
@@ -17,7 +17,7 @@ cfec <- inner_join(cfec, deflationTable)
 cfec <- mutate(cfec, g_price = g_price / defl, g_earn = g_earn / defl)
 cfec$defl <- NULL
 
-groupings <- read.csv("../../data/species-groupings.csv", stringsAsFactors = FALSE)
+groupings <- read.csv("data/species-groupings.csv", stringsAsFactors = FALSE)
 names(groupings) <- tolower(names(groupings))
 names(groupings) <- gsub(" \\(spec\\)", "", names(groupings))
 groupings <- select(groupings, specn, group1)
@@ -27,10 +27,10 @@ cfec <- mutate(cfec, salmon = ifelse(spec %in%
   c("CHNK", "SOCK", "COHO", "PINK", "CHUM"), TRUE, FALSE))
 cfec <- mutate(cfec, pollock = ifelse(spec == "PLCK", TRUE, FALSE))
 
-region <- read.csv("../../data/regions.csv", stringsAsFactors = FALSE)
+region <- read.csv("data/regions.csv", stringsAsFactors = FALSE)
 region <- region[,c(1:2)] #don’t include “assigned by” column
 cfec$region <- NULL
 cfec <- left_join(cfec, region)
 
 # devtools::install_github("wesm/feather/R")
-feather::write_feather(cfec, "../data-generated/cfec.feather")
+feather::write_feather(cfec, "data-generated/cfec.feather")
