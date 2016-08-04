d9c94e8aef04fde99802fd3dc310eb26e7e617e7
eric-ward <eric.ward@noaa.gov>
2016-08-04 09:41:49 -0700

    fig 1 update

--- portfolio/analysis/Figure_1.R
+++ portfolio/analysis/Figure_1.R
@@ -10,19 +10,45 @@ library(date)
 
 cfec <- feather::read_feather("portfolio/data-generated/cfec.feather")
 
+deflationTable <- read.csv("data/deflation.csv")
+
+# Adjust price and g_earn for inflation
+cfec$year <- as.numeric(cfec$year)
+cfec$day <- substr(cfec$landdate, 6, 10)
+cfec <- inner_join(cfec, deflationTable)
+cfec <- mutate(cfec, g_price = g_price / defl, g_earn = g_earn / defl)
+cfec$defl <- NULL
+
+# panel 0 is something like total revenue across fleet
+p0a = group_by(cfec, year) %>%
+  summarize(totEarn = sum(g_earn)/1e9, salmEarn = sum(g_earn[substr(p_fshy,1,1) == "S"])/1e9,
+    nonsalmEarn = sum(g_earn[substr(p_fshy,1,1) != "S"])/1e9) %>%
+  ggplot(aes(year, totEarn, colour = "total"), color = "black") + geom_line() +
+  geom_line(aes(year, salmEarn, colour = "salmon")) +
+  geom_line(aes(year, nonsalmEarn, colour = "non-salmon")) + ylab("Total revenue (billions)") + ggtitle("Total revenue (inflation adjusted)")
+
+p0b = group_by(cfec, year, p_holder) %>%
+  summarize(earn = sum(g_earn), salmearn = sum(gearn[substr(p_fshy,1,1) == "S"])) %>%
+  group_by(year) %>%
+  summarize(median = median(earn), low = quantile(earn, 0.25),
+    upper = quantile(earn, 0.75), tot = sum(earn),
+    people = length(unique(p_holder)), median.salm = median(salmearn)) %>%
+  ggplot(aes(year, log(median))) + geom_ribbon(aes(ymin = log(low), ymax = log(upper)), alpha=0.3, color ="grey") +
+  geom_line() + ylab("Ln median revenue (across people)") + xlab("Year") + ggtitle("Median revenue")
+
 # panel 1 = something like proportion of single permit holders, broken out by salmon
 # this calculates number of salmon and non-salmon permits held per person-year
 permits_person_year = group_by(cfec, p_holder, year) %>%
   summarize(n.otherpermit = length(unique(p_fshy[g_earn>0 & substr(p_fshy,1,1) != "S"])),
     n.salpermit = length(unique(p_fshy[g_earn>0 & substr(p_fshy,1,1) == "S"])))
-permits_person = group_by(year) %>%
+permits_person = group_by(permits_person_year, year) %>%
   summarize(n.other = length(which(n.otherpermit==1 & n.salpermit==0)),
     n.sal = length(which(n.salpermit==1 & n.otherpermit==0)), nTot = n())
 
-p1 = ggplot(permits_person, aes(x = year, y = (n.sal)/nTot)) +
-  geom_line(colour="salmon1",size=2) +
-  geom_line(aes(x = year, y = (n.sal+n.other)/nTot), colour = "black",size=2) +
-    xlab("Proportion of permit holders fishing single permit") + ylab("Year")
+p1 = ggplot(permits_person, aes(x = year, y = (n.sal)/nTot, colour = "salmon")) +
+  geom_line() +
+  geom_line(aes(x = year, y = (n.sal+n.other)/nTot, colour = "total")) +
+    xlab("Year") + ylab("Proportion of total active") + ggtitle("Single permit fishers")
 
 # panel 2 = something like species diversity, maybe again splitting out salmon effect?
 simp.div = function(x) {
@@ -49,56 +75,10 @@ p2 = group_by(effSpec, year) %>%
     lower = mean - sd/mean, upper = mean + sd/mean,
     lower.other = mean.other - sd.other/mean.other,
     upper.other = mean.other + sd.other/mean.other) %>%
-  ggplot(aes(x = year, y = mean)) + geom_line() + geom_line(aes(x = year, y = mean), col="blue", size=2) +
-  geom_line(aes(x = year, y = mean.other), col = "green", size=2) + xlab("Year") + ylab("Effective diversity")
-  #geom_ribbon(aes(ymin = lower, ymax = upper), alpha=0.2) +
-  #geom_ribbon(aes(ymin = lower.other, ymax = upper.other), alpha=0.2)
+  ggplot(aes(x = year, y = mean)) + geom_line() + geom_line(aes(x = year, y = mean, colour = "all")) +
+  geom_line(aes(x = year, y = mean.other, colour = "no salmon specialists")) + xlab("Year") +
+  ylab("Effective species diversity ($ weighted)") + ggtitle("Species diversity")
+
 pdf("portfolio/figs/Fig1.pdf")
-multiplot(p1, p2, cols=2)
+gridExtra::grid.arrange(p0a, p0b, p1, p2, ncol=2)
 dev.off()
-
-# Multiple plot function
-#
-# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
-# - cols:   Number of columns in layout
-# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
-#
-# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
-# then plot 1 will go in the upper left, 2 will go in the upper right, and
-# 3 will go all the way across the bottom.
-#
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  library(grid)
-
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
-
-  numPlots = length(plots)
-
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-      ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-
-  if (numPlots==1) {
-    print(plots[[1]])
-
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-        layout.pos.col = matchidx$col))
-    }
-  }
-}
