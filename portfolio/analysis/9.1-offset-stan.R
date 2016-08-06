8cc8d97cd9ab06f00346d35cb8c2f6184b755f76
Sean Anderson <sean@seananderson.ca>
2016-08-06 14:21:01 -0700

    Tweaks to offset StanR code

--- portfolio/analysis/9.1-offset-stan.R
+++ portfolio/analysis/9.1-offset-stan.R
@@ -1,19 +1,20 @@
+library(dplyr)
 library(rstan)
-options(mc.cores = parallel::detectCores())
+options(mc.cores = 4L)
 rstan_options(auto_write = TRUE)
 
 source("portfolio/analysis/cull-dat.R")
 dat <- cullDat(diff = TRUE)
 # downsample for fast testing
 unique_holders <- unique(dat$p_holder)
-n_sample <- round(length(unique_holders)*0.04)
-set.seed(123)
+n_sample <- round(length(unique_holders)*0.1)
+set.seed(1)
 dat <- dplyr::filter(dat, p_holder %in% base::sample(unique_holders, n_sample))
 nrow(dat)
 
-dat <- dat %>% group_by(strategy) %>% mutate(n=n()) %>% filter(n > 30)
-nrow(dat)
-length(unique(dat$strategy))
+# dat <- dat %>% group_by(strategy) %>% mutate(n=n()) %>% filter(n > 0)
+# nrow(dat)
+# length(unique(dat$strategy))
 
 dat <- mutate(dat,
   revenue_change = log(revenue/revenue.prev),
@@ -44,24 +45,21 @@ standat <- list(
   n_str_yr = length(unique(dat$str_yr_id)),
   str_yr_i = dat$str_yr_id,
 
-  b1_cov_i = dat$scaled_spec_div,
-  # b2_cov_i = dat$log_days_permit,
-  g1_cov_i = dat$scaled_spec_div,
+  b1_cov_i = dat$spec_change,
+  b2_cov_i = dat$days_change,
+  g1_cov_i = dat$spec_change,
 
   mean_div = dat$strategy_mean_div,
-  mean_div_sq = dat$strategy_mean_div^2
+  mean_div_sq = poly(dat$strategy_mean_div, 2)[,2]
   )
 
 # custom tighter inits:
 beta_init <- function() runif(standat$J, -0.1, 0.1)
 sigma_init <- function() runif(standat$K, -0.1, 0.1)
-# start at 0 in case no strategy-level variation:
 dev_str_init <- function() runif(standat$n_strategy, 0, 0)
 dev_yr_init <- function() runif(standat$n_str_yr, 0, 0)
-tau_init <- function() runif(1, 0.3, 0.6)
-
-init_fun <- function(seed = sample(seq_len(1e4), 1)) {
-  set.seed(seed)
+tau_init <- function() runif(1, 0.2, 0.4)
+init_fun <- function() {
   list(
     b0 = beta_init()[1],
     b0_strategy = dev_str_init(),
@@ -69,7 +67,8 @@ init_fun <- function(seed = sample(seq_len(1e4), 1)) {
     b0_strategy_tau = tau_init(),
     b0_str_yr_tau = tau_init(),
     b_j = beta_init(),
-    h = runif(4, -0.1, 0.1),
+    h1 = beta_init()[1],
+    h2 = beta_init()[1],
     b1_strategy = dev_str_init(),
     b1_strategy_tau = tau_init(),
     b2_strategy = dev_str_init(),
@@ -83,5 +82,14 @@ init_fun <- function(seed = sample(seq_len(1e4), 1)) {
 }
 
 m <- stan("portfolio/analysis/portfolio-offset.stan",
-  data = standat, iter = 80, chains = 2,
+  data = standat, iter = 100, chains = 4,
   pars = c("mu", "sigma", "b0_str_yr"), include = FALSE, init = init_fun)
+b <- broom::tidy(m, conf.int = T, estimate.method = "median", rhat = T, ess = T)
+filter(b, rhat > 1.15)
+filter(b, ess < 40)
+filter(b, grepl("^h1", term))
+filter(b, grepl("^b0", term))
+filter(b, grepl("^g0", term))
+filter(b, grepl("^g_k", term))
+filter(b, grepl("^b_j", term))
+filter(b, grepl("*_tau$", term))
