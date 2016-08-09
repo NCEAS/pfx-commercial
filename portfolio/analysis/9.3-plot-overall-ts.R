9bfdf46cb9175d3380aa0ba87f10b5624c07e8a6
Sean Anderson <sean@seananderson.ca>
2016-08-09 13:42:53 -0700

    Rename figure one code and separate data from plots
    Separation is to make iteration of plots faster

--- portfolio/analysis/9.3-plot-overall-ts.R
+++ portfolio/analysis/9.3-plot-overall-ts.R
@@ -6,24 +6,24 @@ library(dplyr)
 library(knitr)
 library(ggplot2)
 library(date)
+devtools::load_all("pfxr")
 
 # cfec.feather already inflation adjusted
-cfec <- feather::read_feather("portfolio/data-generated/cfec.feather")
+if (!exists("cfec")) {
+  cfec <- feather::read_feather("portfolio/data-generated/cfec.feather")
 
-cfec$year <- as.numeric(cfec$year)
+  cfec$year <- as.numeric(cfec$year)
 
-# add indicator for single permit landings
-cfec = group_by(cfec, year, p_holder) %>%
-  mutate(n.permit = length(unique(p_fshy))) %>%
-  mutate(permits = ifelse(n.permit==1, "Single","Multiple")) %>%
-  select(-n.permit)
+  # add indicator for single permit landings
+  cfec = group_by(cfec, year, p_holder) %>%
+    mutate(n.permit = length(unique(p_fshy))) %>%
+    mutate(permits = ifelse(n.permit==1, "Single","Multiple")) %>%
+    select(-n.permit)
+}
 
-p0a = group_by(cfec, year, permits) %>%
+p0a_dat = group_by(cfec, year, permits) %>%
   summarize(totEarn = sum(g_earn)/1e9, salmEarn = sum(g_earn[substr(p_fshy,1,1) == "S"])/1e9,
-    nonsalmEarn = sum(g_earn[substr(p_fshy,1,1) != "S"])/1e9) %>%
-  ggplot(aes(year, totEarn, group = permits, colour = permits)) + geom_line() +
-  ylab("Total revenue (billions)") + ggtitle("Total revenue (inflation adjusted)")
-
+    nonsalmEarn = sum(g_earn[substr(p_fshy,1,1) != "S"])/1e9)
 # panel 0 is something like total revenue across fleet
 #p0a = group_by(cfec, year) %>%
 #  summarize(totEarn = sum(g_earn)/1e9, salmEarn = sum(g_earn[substr(p_fshy,1,1) == "S"])/1e9,
@@ -32,12 +32,11 @@ p0a = group_by(cfec, year, permits) %>%
 #  geom_line(aes(year, salmEarn, colour = "salmon")) +
 #  geom_line(aes(year, nonsalmEarn, colour = "non-salmon")) + ylab("Total revenue (billions)") + ggtitle("Total revenue (inflation adjusted)")
 
-p0b = group_by(cfec, year, p_holder, permits) %>%
+p0b_dat = group_by(cfec, year, p_holder, permits) %>%
   summarize(earn = sum(g_earn), npermit = length(unique(p_fshy))) %>%
   group_by(year, permits) %>%
   summarize(median = median(earn), low = quantile(earn, 0.25),
-    upper = quantile(earn, 0.75)) %>%
-  ggplot(aes(year, log(median), group=permits, color = permits)) + geom_line() + ylab("Ln median revenue (across people)") + xlab("Year") + ggtitle("Median revenue")
+    upper = quantile(earn, 0.75))
 
 # panel 1 = something like proportion of single permit holders, broken out by salmon
 # this calculates number of salmon and non-salmon permits held per person-year
@@ -48,10 +47,7 @@ permits_person = group_by(permits_person_year, year) %>%
   summarize(n.other = length(which(n.otherpermit==1 & n.salpermit==0)),
     n.sal = length(which(n.salpermit==1 & n.otherpermit==0)), nTot = n())
 
-p1 = ggplot(permits_person, aes(x = year, y = (n.sal)/nTot, colour = "salmon")) +
-  geom_line() +
-  geom_line(aes(x = year, y = (n.sal+n.other)/nTot, colour = "total")) +
-    xlab("Year") + ylab("Proportion of total active") + ggtitle("Single permit fishers")
+p1_dat <- permits_person
 
 # panel 2 = something like species diversity, maybe again splitting out salmon effect?
 simp.div = function(x) {
@@ -70,18 +66,54 @@ effSpec$salmon = 0 # ugly, wasn't working with dplyr
 effSpec$salmon[paste(effSpec$p_holder,effSpec$year)%in%paste(salmon_specialists$p_holder,salmon_specialists$year)] = 1
 
 # showing uncertainty on this plot isn't very useful (variability across people)
-p2 = group_by(effSpec, year) %>%
+p2_dat = group_by(effSpec, year) %>%
   summarize(mean = mean(effSpec,na.rm=T),
     mean.other = mean(effSpec.nosal[is.finite(effSpec.nosal)],na.rm=T),
     sd = sd(effSpec,na.rm=T),
     sd.other = sd(effSpec.nosal[is.finite(effSpec.nosal)],na.rm=T),
     lower = mean - sd/mean, upper = mean + sd/mean,
     lower.other = mean.other - sd.other/mean.other,
-    upper.other = mean.other + sd.other/mean.other) %>%
-  ggplot(aes(x = year, y = mean)) + geom_line() + geom_line(aes(x = year, y = mean, colour = "all")) +
-  geom_line(aes(x = year, y = mean.other, colour = "no salmon specialists")) + xlab("Year") +
-  ylab("Effective species diversity ($ weighted)") + ggtitle("Species diversity")
+    upper.other = mean.other + sd.other/mean.other)
 
-pdf("portfolio/figs/Fig1b.pdf")
+# --------------------------------------------
+this_fig_theme <- theme_gg() +
+  # theme(legend.justification = c(1, 1), legend.position = c(1, 1))
+  theme(legend.position="none")
+
+p0a <-  p0a_dat %>%
+  ggplot(aes(year, totEarn, group = permits, colour = permits)) +
+  geom_line() +
+  ylab("Total revenue (billions)") +
+  # ggtitle("Total revenue (inflation adjusted)") +
+  this_fig_theme
+
+p0b <- p0b_dat %>%
+  ggplot(aes(year, median, group=permits, color = permits)) +
+  geom_line() +
+  ylab("Median revenue/permit") +
+  xlab("Year") +
+  scale_y_log10() +
+  # ggtitle("Median revenue") +
+  this_fig_theme
+
+p1 <- p1_dat %>%
+  ggplot(aes(x = year, y = (n.sal)/nTot, colour = "salmon")) +
+  geom_line() +
+  geom_line(aes(x = year, y = (n.sal+n.other)/nTot, colour = "total")) +
+  xlab("Year") +
+  ylab("Proportion of total active") +
+  # ggtitle("Single permit fishers") +
+  this_fig_theme
+
+p2 <- p2_dat %>%
+  ggplot(aes(x = year, y = mean)) + geom_line() +
+  geom_line(aes(x = year, y = mean, colour = "all")) +
+  geom_line(aes(x = year, y = mean.other, colour = "no salmon specialists")) +
+  xlab("Year") +
+  ylab("Effective species diversity") +
+  # ggtitle("Species diversity") +
+  this_fig_theme
+
+pdf("portfolio/figs/fig1.pdf", width = 6.5, height = 4.75)
 gridExtra::grid.arrange(p0a, p0b, p1, p2, ncol=2)
 dev.off()
