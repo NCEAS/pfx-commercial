9c13d5cca070aa419961dcd2ac9d20463ae4acd0
Sean Anderson <sean@seananderson.ca>
2016-08-13 14:34:36 -0700

    Fix map and tidy code (including downsampling)

--- portfolio/analysis/3.4-source-all-figs.R
+++ portfolio/analysis/3.4-source-all-figs.R
@@ -1,9 +1,6 @@
 # convenience script
 
-plot_fig1 <- FALSE
-
 f <- list.files(path = file.path("portfolio", "analysis"), pattern = "^3\\.3-*")
-if (!plot_fig1) f <- f[!grepl("map", f)]
 f <- c(f, list.files(path = file.path("portfolio", "analysis"), pattern = "*check-*"))
 f <- file.path("portfolio", "analysis", f)
 
