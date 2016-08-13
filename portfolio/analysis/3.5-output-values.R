e0a75529925ef2926564522e9d2a9ead577c0d61
Sean Anderson <sean@seananderson.ca>
2016-08-13 15:28:55 -0700

    Integrate output values

---
+++ portfolio/analysis/3.5-output-values.R
@@ -0,0 +1,39 @@
+rm(list = ls())
+source("portfolio/analysis/prep-stan-model-matrix.R")
+load("portfolio/data-generated/fig1-dat.rda")
+
+library(dplyr)
+
+nrows <- nrow(mm)
+npholders <- length(unique(dat$p_holder))
+range_yrs <- range(dat$year)
+nstr <- length(unique(dat$strategy))
+rvers <- paste(R.version$major, R.version$minor, sep = ".")
+
+prop_single_all <- filter(out_permits, jk == "x")$prop_single
+min_prop_single <- min(prop_single_all)
+yr_min_prop_single <- filter(out_permits, jk == "x")$year[prop_single_x == min_prop_single]
+min_prop_single <- min_prop_single %>% round(2) * 100
+max_prop_single <- filter(out_permits, jk == "x")$prop_single %>%
+  max() %>% round(2) * 100
+curr_prop_single_nohal <- filter(out_permits,
+  jk == "B")$prop_single[length(unique(out_permits$year))] %>% round(2) * 100
+curr_prop_single_nosalm <- filter(out_permits,
+  jk == "S")$prop_single[length(unique(out_permits$year))] %>% round(2) * 100
+
+spdiv_all <- filter(out_div, jk == "x")$mean_spdiv
+max_spdiv_all <- max(spdiv_all)
+yr_max_spdiv_all <- filter(out_div, jk == "x")$year[spdiv_all == max_spdiv_all]
+max_spdiv_all <- max_spdiv_all %>% round(2) * 100
+
+min_spdiv_all <- min(spdiv_all)
+yr_min_spdiv_all <- filter(out_div, jk == "x")$year[spdiv_all == min_spdiv_all]
+min_spdiv_all <- min_spdiv_all %>% round(2) * 100
+
+spdiv_salm <- filter(out_div, jk == "S")$mean_spdiv
+max_spdiv_salm <- max(spdiv_salm)
+yr_max_spdiv_salm <- filter(out_div, jk == "S")$year[spdiv_salm == max_spdiv_salm]
+max_spdiv_salm <- max_spdiv_salm %>% round(2) * 100
+
+rm(cullDat, mm, mm2, dat, unique_holders)
+save.image(file = "portfolio/data-generated/output-values.rda")
