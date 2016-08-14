ed57c9cd2effb7f8a94a985fc51e15f6ba7a8e0d
Sean Anderson <sean@seananderson.ca>
2016-08-13 17:07:32 -0700

    Tweaks to output values

--- portfolio/analysis/3.5-output-values.R
+++ portfolio/analysis/3.5-output-values.R
@@ -4,6 +4,9 @@ load("portfolio/data-generated/fig1-dat.rda")
 
 library(dplyr)
 
+nums <- c("one" = 1, "two" = 2, "three" = 3, "four" = 4, "five" = 5, "six" = 6,
+  "seven" = 7, "eight" = 8, "nine" = 9, "ten" = 10, "11" = 11, "12" = 12, "13" = 13)
+
 nrows <- nrow(mm)
 npholders <- length(unique(dat$p_holder))
 range_yrs <- range(dat$year)
@@ -12,7 +15,7 @@ rvers <- paste(R.version$major, R.version$minor, sep = ".")
 
 prop_single_all <- filter(out_permits, jk == "x")$prop_single
 min_prop_single <- min(prop_single_all)
-yr_min_prop_single <- filter(out_permits, jk == "x")$year[prop_single_x == min_prop_single]
+yr_min_prop_single <- filter(out_permits, jk == "x")$year[prop_single_all == min_prop_single]
 min_prop_single <- min_prop_single %>% round(2) * 100
 max_prop_single <- filter(out_permits, jk == "x")$prop_single %>%
   max() %>% round(2) * 100
@@ -34,6 +37,9 @@ spdiv_salm <- filter(out_div, jk == "S")$mean_spdiv
 max_spdiv_salm <- max(spdiv_salm)
 yr_max_spdiv_salm <- filter(out_div, jk == "S")$year[spdiv_salm == max_spdiv_salm]
 max_spdiv_salm <- max_spdiv_salm %>% round(2) * 100
+
+n_jacknifed <- length(unique(out_div$jk)) - 1
+n_jacknifed <- names(nums[n_jacknifed])
 
 rm(cullDat, mm, mm2, dat, unique_holders)
 save.image(file = "portfolio/data-generated/output-values.rda")
