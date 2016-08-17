f3ec3aeee44ae416a3589c43d7bbb9693d30e9bb
Sean Anderson <sean@seananderson.ca>
2016-08-17 14:08:46 -0700

    Tweaks to lmer-stan comparison

--- portfolio/analysis/3.3-lmer-stan-comparison.R
+++ portfolio/analysis/3.3-lmer-stan-comparison.R
@@ -11,7 +11,9 @@ b2 <- function(x, bp = 0) ifelse(x < bp, 0, x)
 m_lmer <- lmer(
   log(revenue) ~ -1 + b1(spec_change) + b2(spec_change) +
     days_change + b1(spec_change):days_change + b2(spec_change):days_change +
-    (-1 + b1(spec_change) + b2(spec_change) |strategy) + (1|strategy_year),
+    (-1 + b1(spec_change) |strategy) +
+    (-1 + b2(spec_change) |strategy)+
+    (1|strategy_year),
   data = dat, offset = log(revenue.prev))
 
 re_lmer <- coef(m_lmer)$strategy
@@ -36,7 +38,8 @@ b1 <- tidyr::gather(p$coef_b1_strategy, term, posterior) %>%
   mutate(parameter = "b1")
 b2 <- tidyr::gather(p$coef_b2_strategy, term, posterior) %>%
   mutate(parameter = "b2")
-po <- bind_rows(g0, g1, g2, b1, b2)
+# po <- bind_rows(g0, g1, g2, b1, b2)
+po <- bind_rows(b1, b2)
 
 pos <- group_by(po, term, parameter) %>% summarise(
   l = quantile(posterior, probs = 0.025),
@@ -57,10 +60,11 @@ b2 <- filter(pos, parameter == "b2") %>%
 bs <- bind_rows(b1, b2)
 
 g <- ggplot(bs, aes(blmer, y = m)) +
-  geom_point(alpha = 0.6, size = 0.9) +
+  geom_point(alpha = 0.6, size = 1.1) +
   geom_segment(aes(x = blmer, xend = blmer, y = l.5, yend = u.5), lwd = 1, alpha = 0.5) +
   geom_segment(aes(x = blmer, xend = blmer, y = l, yend = u), lwd = 0.2, alpha = 0.5) +
   coord_flip() +
+  # coord_fixed() +
   facet_wrap(~parameter) +
   geom_abline(intercept = 0, slope = 1, lty = 2) +
   theme_gg() +
@@ -68,4 +72,4 @@ g <- ggplot(bs, aes(blmer, y = m)) +
   geom_vline(xintercept = 0, lty = 2) +
   xlab("lme4 estimate") +
   ylab("Stan estimate")
-ggsave("portfolio/figs/lmer-vs-stan.pdf", width = 7, height = 3.4)
+ggsave("portfolio/figs/lmer-vs-stan2.pdf", width = 7, height = 3.4)
