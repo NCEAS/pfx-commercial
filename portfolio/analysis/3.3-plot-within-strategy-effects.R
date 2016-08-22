f35f5ef69068c4a589e1c49a75b68630f542feef
Sean Anderson <sean@seananderson.ca>
2016-08-22 16:04:15 -0700

    Add CIs to last plot

--- portfolio/analysis/3.3-plot-within-strategy-effects.R
+++ portfolio/analysis/3.3-plot-within-strategy-effects.R
@@ -7,7 +7,7 @@ load("portfolio/data-generated/m.rda")
 devtools::load_all("pfxr")
 
 b <- broom::tidyMCMC(m, conf.int = T, estimate.method = "median",
-  conf.level = 0.8, conf.method = "quantile")
+  conf.level = 0.5, conf.method = "quantile")
 
 g1 <- b[grepl("coef_g1", b$term), ]
 b1 <- b[grepl("coef_b1", b$term), ]
@@ -22,16 +22,30 @@ res$dec <- g1$estimate
 res$inc_rev <- b2$estimate
 res$dec_rev <- b1$estimate
 
+res$inc.l <- g2$conf.low
+res$dec.l <- g1$conf.low
+res$inc_rev.l <- b2$conf.low
+res$dec_rev.l <- b1$conf.low
+
+res$inc.u <- g2$conf.high
+res$dec.u <- g1$conf.high
+res$inc_rev.u <- b2$conf.high
+res$dec_rev.u <- b1$conf.high
+
 ns <- dat %>% group_by(strategy) %>%
   summarise(nn = length(unique(p_holder)))
 res <- inner_join(res, ns)
 
 p1 <- res %>%
-  mutate(strategy_label = ifelse(nn > 100, str_label, NA)) %>%
+  mutate(strategy_label = ifelse(nn > 20, str_label, NA)) %>%
   mutate(inc = inc) %>%
   ggplot(aes(x = inc_rev, y = inc)) +
   geom_hline(yintercept = 0, lty = 2, col = "grey65") +
   geom_vline(xintercept = 0, lty = 2, col = "grey65") +
+  geom_segment(aes(x = inc_rev.l, xend = inc_rev.u, y = inc, yend = inc),
+    alpha = 0.2, lwd = 0.4) +
+  geom_segment(aes(x = inc_rev, xend = inc_rev, y = inc.l, yend = inc.u),
+    alpha = 0.2, lwd = 0.4) +
   geom_text_repel(aes(label = strategy_label, x = inc_rev, y = inc),
     size = 2.8, colour = "grey60", segment.color = "grey80",
     point.padding = unit(0.3, "lines"), max.iter = 6e3, segment.size = 0.3) +
@@ -50,10 +64,16 @@ p1 <- res %>%
 
 p2 <- res %>%
   mutate(dec = -dec, dec_rev = -dec_rev) %>%
-  mutate(strategy_label = ifelse(nn > 100, str_label, NA)) %>%
+  mutate(dec.l = -dec.l, dec_rev.l = -dec_rev.l) %>%
+  mutate(dec.u = -dec.u, dec_rev.u = -dec_rev.u) %>%
+  mutate(strategy_label = ifelse(nn > 20, str_label, NA)) %>%
   ggplot(aes(x = dec_rev, y = dec)) +
   geom_hline(yintercept = 0, lty = 2, col = "grey65") +
   geom_vline(xintercept = 0, lty = 2, col = "grey65") +
+  geom_segment(aes(x = dec_rev.l, xend = dec_rev.u, y = dec, yend = dec),
+    alpha = 0.2, lwd = 0.4) +
+  geom_segment(aes(x = dec_rev, xend = dec_rev, y = dec.l, yend = dec.u),
+    alpha = 0.2, lwd = 0.4) +
   geom_text_repel(aes(label = strategy_label, x = dec_rev, y = dec),
     size = 2.8, colour = "grey60", segment.color = "grey80",
     point.padding = unit(0.3, "lines"), max.iter = 6e3, segment.size = 0.3) +
@@ -61,7 +81,7 @@ p2 <- res %>%
   xlab("Effect of specializing on revenue") +
   ylab("Effect of specializing on variability") +
   scale_color_viridis() +
-  annotate("text", x = min(res$inc_rev), y = max(res$inc), label = "B",
+  annotate("text", x = min(-1*res$dec_rev), y = max(-1*res$dec), label = "B",
     fontface = "bold", size = 5) +
   theme_gg()
 
