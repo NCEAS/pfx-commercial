4b9d589e99955d7526bffeba3a664fc263e8f837
eric-ward <eric.ward@noaa.gov>
2016-09-27 16:56:28 -0700

    avg halibut prices, updated plots

--- portfolio/analysis/98 appendixFigure_permitValues.R
+++ portfolio/analysis/98 appendixFigure_permitValues.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(ggplot2)
 
-d = read.csv("portfolio/data-generated/SEAK_pemitValues_CFEC.csv")
+d = read.csv("data/SEAK_pemitValues_CFEC.csv")
 
 d = d[,c("PERIOD","VALUE","Permit")]
 names(d)[which(names(d)=="PERIOD")]="year"
@@ -20,5 +20,23 @@ g1 = ggplot(d, aes(year, value/1000, color = Permit)) + geom_line() + xlab("Year
 g2 = ggplot(d, aes(year, rel, color = Permit)) + geom_line() + xlab("Year") + ylab("% change relative to 2000 value") + theme_bw()
 
 png("portfolio/figs/permit_values.png")
+gridExtra::grid.arrange(g1, g2, ncol=1)
+dev.off()
+
+
+d = read.csv("data/halibut_prices.csv")
+names(defl)[which(names(defl)=="year")]="Year"
+d = left_join(d, defl)
+d$value = d$Price/d$defl
+
+d = group_by(d, Quota) %>%
+  mutate(rel = value/value[which(Year==2000)])
+
+g1 = ggplot(d, aes(Year, value, color = Quota)) + geom_line() + xlab("Year") + ylab("Average price ($)") + theme_bw()
+
+# Also calculate version of relative prices, relative to 2000
+g2 = ggplot(d, aes(Year, rel, color = Quota)) + geom_line() + xlab("Year") + ylab("% change relative to 2000 value") + theme_bw()
+
+png("portfolio/figs/halibut_quota_price.png")
 gridExtra::grid.arrange(g1, g2, ncol=1)
 dev.off()
