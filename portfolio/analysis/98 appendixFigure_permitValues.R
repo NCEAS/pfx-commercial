e175f9408a046310cd278ccc670909198df8a332
eric-ward <eric.ward@noaa.gov>
2016-09-27 15:32:44 -0700

    appendix plot for permit values

---
+++ portfolio/analysis/98 appendixFigure_permitValues.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(ggplot2)
+
+d = read.csv("portfolio/data-generated/SEAK_pemitValues_CFEC.csv")
+
+d = d[,c("PERIOD","VALUE","Permit")]
+names(d)[which(names(d)=="PERIOD")]="year"
+
+defl = read.csv("data/deflation.csv")
+
+d = left_join(d, defl)
+d$value = d$VALUE/d$defl
+
+d = group_by(d, Permit) %>%
+  mutate(rel = value/value[which(year==2000)])
+
+g1 = ggplot(d, aes(year, value/1000, color = Permit)) + geom_line() + xlab("Year") + ylab("Estimated value ($1000s)") + theme_bw()
+
+# Also calculate version of relative prices, relative to 2000
+g2 = ggplot(d, aes(year, rel, color = Permit)) + geom_line() + xlab("Year") + ylab("% change relative to 2000 value") + theme_bw()
+
+png("portfolio/figs/permit_values.png")
+gridExtra::grid.arrange(g1, g2, ncol=1)
+dev.off()
