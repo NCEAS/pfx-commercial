2a30a5996cd83d20a915cf2fcafd04d84bab36b4
eric-ward <eric.ward@noaa.gov>
2016-10-13 20:09:46 -0700

    update

--- summaries_for_ole.R
+++ summaries_for_ole.R
@@ -3,10 +3,9 @@
 cfec <- feather::read_feather("portfolio/data-generated/cfec.feather")
 cfec$year <- as.numeric(cfec$year)
 
-areas = read.csv(file.choose())
-
-cfec = cfec[which(cfec$stat6 %in% areas$STAT6),]
 cfec = cfec[which(substr(cfec$p_fshy,1,1) %in% c("M","B","C")),]
+
+cfec = cfec[which(cfec$region%in%c("Kodiak","PWS","Alaska Peninsula","Cook Inlet")),]
 
 # 99.4% of the records are made up of B/C/M permits, 361619 / 363789
 library(dplyr)
@@ -14,10 +13,9 @@ g = group_by(cfec, year, stat6) %>%
   summarize(pounds = sum(g_pounds))
 
 # roll these stat6 areas into Ole's regions
-names(areas)[which(names(areas)=="STAT6")] = "stat6"
-g = left_join(g, areas)
+#names(areas)[which(names(areas)=="STAT6")] = "stat6"
+#g = left_join(g, areas)
+#totPounds = group_by(g, AREA, year) %>%
+#  summarize(totPounds = sum(pounds))
 
-totPounds = group_by(g, AREA, year) %>%
-  summarize(totPounds = sum(pounds))
-
-write.table(totPounds, "totPounds.csv", sep=",", row.names=FALSE)
+write.table(g, "pounds_by_stat6.csv", sep=",", row.names=FALSE)
