bf7c8afd7308f4678df1d5168170d46d93109380
Sean Anderson <sean@seananderson.ca>
2016-12-05 18:12:57 -0800

    Create and add IFQ figure to supplement

--- portfolio/analysis/3.3-compare-ifq.R
+++ portfolio/analysis/3.3-compare-ifq.R
@@ -56,8 +56,8 @@ res_ifq <- mutate(res_ifq,
 # ----------
 # plot the effects between the 2 models
 
-plot_ifq_effect <- function(variable = "inc", title = "", flip = FALSE,
-  confidence_intervals = FALSE) {
+plot_ifq_effect <- function(variable = "inc", title = "", flip = FALSE, lab = "",
+  confidence_intervals = FALSE, cols = RColorBrewer::brewer.pal(8, "Greys")[c(4, 8)]) {
   d1 <- select_(res, "strategy", variable) %>%
     rename_(effect = variable)
   d2 <- select_(res_ifq, "strategy_ifq", "strategy", variable) %>%
@@ -65,7 +65,8 @@ plot_ifq_effect <- function(variable = "inc", title = "", flip = FALSE,
   d <- left_join(d1, d2) %>%
     group_by(strategy) %>%
     mutate(ifq = grepl("IFQ", strategy_ifq)) %>%
-    mutate(ifq_any = any(grepl("IFQ", strategy_ifq)))
+    mutate(ifq_any = any(grepl("IFQ", strategy_ifq))) %>%
+    left_join(select(md, strategy, str_label))
 
   if (flip) {
     d$effect <- -1 * d$effect
@@ -74,20 +75,31 @@ plot_ifq_effect <- function(variable = "inc", title = "", flip = FALSE,
 
   d <- d %>%
     filter(ifq_any)
-  g <- ggplot(d, aes(x = "Grouped", xend = "IFQ split", y = effect, yend = effect_ifq,
-    color = ifq)) + geom_segment() +
-    geom_text(data = unique(select(d, effect, strategy)),
-      aes(x = "Grouped", y = effect, label = strategy),
-      hjust = "right", color = "black", inherit.aes = FALSE) +
-    labs(title = title, color = "IFQ") +
-    theme_light()
+  g <- ggplot(d, aes(x = 1, xend = 2, y = effect, yend = effect_ifq,
+    color = ifq, linetype = ifq)) + geom_segment() +
+    geom_text(data = unique(select(d, effect, strategy, str_label)),
+      aes(x = 1, y = effect, label = str_label),
+      hjust = "right", color = "black", inherit.aes = FALSE, size = 3) +
+    labs(color = "IFQ", linetype = "IFQ") +
+    scale_color_manual(values = cols) +
+    scale_linetype_manual(values = c(1, 1)) +
+    theme_light() +
+    scale_x_continuous(limits = c(-0.5, 2.3), breaks = c(1, 2), labels = c("Grouped", "IFQ split")) +
+    ylab(lab) +
+    xlab("") +
+    annotate("text", y = 2.9, x = -0.5, label = "B",
+      fontface = "bold", size = 5)
+    # theme(legend.position=c(.9,.85))
   if (confidence_intervals) {
     warning("Not implemented")
   }
+  g
 }
 
+
 p0 <- plot_ifq_effect("g0", "Variability intercept")
-p1 <- plot_ifq_effect("inc", "Generalizing - variability")
+p1 <- plot_ifq_effect("inc", "", lab = "Effect of generalizing on variability")
+p1
 p2 <- plot_ifq_effect("dec", "Specializing  - variability", flip = TRUE)
 p3 <- plot_ifq_effect("inc_rev", "Generalizing  - revenue")
 p4 <- plot_ifq_effect("dec_rev", "Specializing  - revenue", flip = TRUE)
@@ -99,77 +111,77 @@ dev.off()
 # ----------
 # plot the effects just within the model that splits ifqs
 
-plot_ifq_effect2 <- function(variable = "inc", title = "", flip = FALSE) {
-
-  d2 <- select_(res_ifq, "strategy_ifq", "strategy", variable) %>%
-    rename_(effect = variable)
-  d <- d2 %>%
-    group_by(strategy) %>%
-    mutate(ifq = grepl("IFQ", strategy_ifq)) %>%
-    mutate(ifq_any = any(grepl("IFQ", strategy_ifq))) %>%
-    mutate(salmon = grepl("S", strategy_ifq))
-
-  if (flip) {
-    d$effect <- -1 * d$effect
-  }
-
-  d <- d %>%
-    filter(ifq_any)
-
-  select(d, -strategy_ifq) %>%
-  spread(ifq, effect) %>%
-  ggplot(aes(x = "Before-IFQ", xend = "IFQ", y = `FALSE`, yend = `TRUE`)) +
-    geom_segment(aes(color = salmon)) +
-    geom_text(aes(x = "IFQ", y = `TRUE`, label = strategy),
-      hjust = "left", color = "black", inherit.aes = FALSE) +
-    labs(title = title, y = "Effect", x = "") +
-    theme_light()
-}
-
-p0 <- plot_ifq_effect2("g0", "Variability intercept")
-p1 <- plot_ifq_effect2("inc", "Generalizing - variability")
-p2 <- plot_ifq_effect2("dec", "Specializing  - variability", flip = TRUE)
-p3 <- plot_ifq_effect2("inc_rev", "Generalizing  - revenue")
-p4 <- plot_ifq_effect2("dec_rev", "Specializing  - revenue", flip = TRUE)
-
-pdf("portfolio/figs/ifq-within-comparison-no-grouped.pdf", width = 10, height = 9)
-gridExtra::grid.arrange(p1, p2, p3, p4, ncol = 2, nrow = 2)
-dev.off()
-
-mean_diversity <- dat %>%
-  group_by(strategy) %>%
-  summarize(mean_diversity = mean(specDiv)) %>%
-  ungroup()
-
-mean_diversity_ifq <- dat %>%
-  group_by(strategy_ifq, strategy) %>%
-  summarize(mean_diversity_ifq = mean(specDiv)) %>%
-  ungroup()
-
-mean_diversity <- inner_join(mean_diversity, mean_diversity_ifq)
-
-mean_diversity <- mean_diversity %>%
-  group_by(strategy) %>%
-  mutate(ifq = grepl("IFQ", strategy_ifq)) %>%
-  mutate(ifq_any = any(grepl("IFQ", strategy_ifq))) %>%
-  mutate(salmon = grepl("S", strategy_ifq)) %>%
-  filter(ifq_any) %>%
-  ungroup()
-
-p0 <- plot_ifq_effect2("g0", "Variability intercept")
-
-p0_diversity <- select(mean_diversity, -strategy_ifq, -mean_diversity) %>%
-  spread(ifq, mean_diversity_ifq) %>%
-  ggplot(aes(x = "Before-IFQ", xend = "IFQ", y = `FALSE`, yend = `TRUE`)) +
-    geom_segment(aes(color = salmon)) +
-    geom_text(aes(x = "IFQ", y = `TRUE`, label = strategy),
-      hjust = "left", color = "black", inherit.aes = FALSE) +
-    labs(title = "Mean diversity", y = "Mean diversity", x = "") +
-    theme_light()
-
-pdf("portfolio/figs/ifq-g0s.pdf", width = 4.5, height = 7)
-gridExtra::grid.arrange(p0, p0_diversity)
-dev.off()
+## plot_ifq_effect2 <- function(variable = "inc", title = "", flip = FALSE) {
+##
+##   d2 <- select_(res_ifq, "strategy_ifq", "strategy", variable) %>%
+##     rename_(effect = variable)
+##   d <- d2 %>%
+##     group_by(strategy) %>%
+##     mutate(ifq = grepl("IFQ", strategy_ifq)) %>%
+##     mutate(ifq_any = any(grepl("IFQ", strategy_ifq))) %>%
+##     mutate(salmon = grepl("S", strategy_ifq))
+##
+##   if (flip) {
+##     d$effect <- -1 * d$effect
+##   }
+##
+##   d <- d %>%
+##     filter(ifq_any)
+##
+##   select(d, -strategy_ifq) %>%
+##   spread(ifq, effect) %>%
+##   ggplot(aes(x = "Before-IFQ", xend = "IFQ", y = `FALSE`, yend = `TRUE`)) +
+##     geom_segment(aes(color = salmon)) +
+##     geom_text(aes(x = "IFQ", y = `TRUE`, label = strategy),
+##       hjust = "left", color = "black", inherit.aes = FALSE) +
+##     labs(title = title, y = "Effect", x = "") +
+##     theme_light()
+## }
+##
+## p0 <- plot_ifq_effect2("g0", "Variability intercept")
+## p1 <- plot_ifq_effect2("inc", "Generalizing - variability")
+## p2 <- plot_ifq_effect2("dec", "Specializing  - variability", flip = TRUE)
+## p3 <- plot_ifq_effect2("inc_rev", "Generalizing  - revenue")
+## p4 <- plot_ifq_effect2("dec_rev", "Specializing  - revenue", flip = TRUE)
+##
+## pdf("portfolio/figs/ifq-within-comparison-no-grouped.pdf", width = 10, height = 9)
+## gridExtra::grid.arrange(p1, p2, p3, p4, ncol = 2, nrow = 2)
+## dev.off()
+##
+## mean_diversity <- dat %>%
+##   group_by(strategy) %>%
+##   summarize(mean_diversity = mean(specDiv)) %>%
+##   ungroup()
+##
+## mean_diversity_ifq <- dat %>%
+##   group_by(strategy_ifq, strategy) %>%
+##   summarize(mean_diversity_ifq = mean(specDiv)) %>%
+##   ungroup()
+##
+## mean_diversity <- inner_join(mean_diversity, mean_diversity_ifq)
+##
+## mean_diversity <- mean_diversity %>%
+##   group_by(strategy) %>%
+##   mutate(ifq = grepl("IFQ", strategy_ifq)) %>%
+##   mutate(ifq_any = any(grepl("IFQ", strategy_ifq))) %>%
+##   mutate(salmon = grepl("S", strategy_ifq)) %>%
+##   filter(ifq_any) %>%
+##   ungroup()
+##
+## p0 <- plot_ifq_effect2("g0", "Variability intercept")
+##
+## p0_diversity <- select(mean_diversity, -strategy_ifq, -mean_diversity) %>%
+##   spread(ifq, mean_diversity_ifq) %>%
+##   ggplot(aes(x = "Before-IFQ", xend = "IFQ", y = `FALSE`, yend = `TRUE`)) +
+##     geom_segment(aes(color = salmon)) +
+##     geom_text(aes(x = "IFQ", y = `TRUE`, label = strategy),
+##       hjust = "left", color = "black", inherit.aes = FALSE) +
+##     labs(title = "Mean diversity", y = "Mean diversity", x = "") +
+##     theme_light()
+##
+## pdf("portfolio/figs/ifq-g0s.pdf", width = 4.5, height = 7)
+## gridExtra::grid.arrange(p0, p0_diversity)
+## dev.off()
 
 # So it looks like the variability intercept is lower with
 # ifqs especially for those strategies that don't have diversity from
@@ -215,10 +227,17 @@ out$str_label_ordered <- NULL
 out <- inner_join(out, labs)
 
 g <- ggplot(out, aes(str_label_ordered, ratio)) +
-  geom_violin() +
-  geom_hline(yintercept = 1) +
+  geom_hline(yintercept = 1, col = "grey60") +
+  geom_violin(fill = "grey95") +
   scale_y_log10(breaks = c(0.6, 0.8, 1, 1.2, 1.4, 1.6)) +
-  xlab("After IFQ/Before IFQ variability") +
+  xlab("After IFQ / Before IFQ variability") +
   coord_flip() +
-  theme_light()
+  theme_light() +
+  annotate("text", y = 0.55, x = 9.2, label = "A",
+    fontface = "bold", size = 5)
+# g
 ggsave("portfolio/figs/itq-ratios.pdf", width = 4.5, height = 4)
+
+pdf("portfolio/figs/itq-combined.pdf", width = 9.5, height = 4.5)
+gridExtra::grid.arrange(g, p1, layout_matrix = as.matrix(t(c(1, 1, 1, 2, 2, 2))))
+dev.off()
