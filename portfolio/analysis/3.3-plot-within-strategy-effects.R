dcd2910222426a8eadea5544694a8bb54080b5c4
Sean Anderson <sean@seananderson.ca>
2016-12-07 18:36:58 -0800

    Make new fig4

--- portfolio/analysis/3.3-plot-within-strategy-effects.R
+++ portfolio/analysis/3.3-plot-within-strategy-effects.R
@@ -37,7 +37,7 @@ ns <- dat %>% group_by(strategy) %>%
 res <- inner_join(res, ns)
 
 p1 <- res %>%
-  mutate(strategy_label = ifelse(nn > 10, str_label, NA)) %>%
+  mutate(strategy_label = ifelse(nn > 250, str_label, NA)) %>%
   mutate(inc = inc) %>%
   ggplot(aes(x = inc_rev, y = inc)) +
   geom_hline(yintercept = 0, lty = 2, col = "grey65") +
@@ -51,7 +51,7 @@ p1 <- res %>%
     point.padding = unit(0.3, "lines"), max.iter = 6e3, segment.size = 0.3) +
   geom_point(aes(color = strategy_mean_div, size = nn)) +
   xlab("Effect of generalizing on revenue") +
-  ylab("Effect of generalizing on variability") +
+  ylab("Effect of generalizing on revenue variability") +
   scale_color_viridis() +
   theme_gg() +
   guides(
@@ -62,11 +62,12 @@ p1 <- res %>%
   labs(colour = "Mean sp.\ndiversity", size = "Number of\npermit holders") +
   theme(legend.title = element_text(size = rel(0.85)))
 
+ann_col <- "grey40"
 p2 <- res %>%
   mutate(dec = -dec, dec_rev = -dec_rev) %>%
   mutate(dec.l = -dec.l, dec_rev.l = -dec_rev.l) %>%
   mutate(dec.u = -dec.u, dec_rev.u = -dec_rev.u) %>%
-  mutate(strategy_label = ifelse(nn > 10, str_label, NA)) %>%
+  mutate(strategy_label = ifelse(nn > 250, str_label, NA)) %>%
   ggplot(aes(x = dec_rev, y = dec)) +
   geom_hline(yintercept = 0, lty = 2, col = "grey65") +
   geom_vline(xintercept = 0, lty = 2, col = "grey65") +
@@ -75,29 +76,40 @@ p2 <- res %>%
   geom_segment(aes(x = dec_rev, xend = dec_rev, y = dec.l, yend = dec.u),
     alpha = 0.1, lwd = 0.4) +
   geom_text_repel(aes(label = strategy_label, x = dec_rev, y = dec),
-    size = 2.8, colour = "grey60", #segment.color = "grey80",
+    size = 2.9, colour = "grey60", #segment.color = "grey80",
     point.padding = unit(0.3, "lines"), max.iter = 6e3, segment.size = 0.3) +
   geom_point(aes(color = strategy_mean_div, size = nn)) +
-  xlab("Effect of specializing on revenue") +
+  xlab("Effect of specializing on revenue variability") +
   ylab("Effect of specializing on variability") +
   scale_color_viridis() +
-  annotate("text", x = min(-1*res$dec_rev.u), y = max(-1*res$dec.l), label = "B",
-    fontface = "bold", size = 5) +
+  labs(colour = "Mean sp.\ndiversity", size = "Number of\npermit holders") +
+  theme(legend.title = element_text(size = rel(0.85))) +
   theme_gg()
 
+p3 <- p2 + annotate("text", x = min(-1*res$dec_rev.u), y = max(-1*res$dec.l), label = "B",
+  fontface = "bold", size = 5)
+
+gap <- 0.15
+left <- -0.7
+right <- 1.4
+p4 <- p2 +
+  annotate("text", x = right, y = 2, label = "' '%up%variability", hjust = 0, col = ann_col, parse = TRUE) +
+  annotate("text", x = right, y = 2-gap, label = "' '%up%revenue", hjust = 0, col = ann_col, parse = TRUE) +
+
+  annotate("text", x = right, y = -0.4, label = "' '%down%variability", hjust = 0, col = ann_col, parse = TRUE) +
+  annotate("text", x = right, y = -0.4-gap, label = "' '%up%revenue", hjust = 0, col = ann_col, parse = TRUE) +
+
+  annotate("text", x = left, y = -0.4, label = "' '%down%variability", hjust = 0, col = ann_col, parse = TRUE) +
+  annotate("text", x = left, y = -0.4-gap, label = "' '%down%revenue", hjust = 0, col = ann_col, parse = TRUE) +
+
+  annotate("text", x = left, y = 2, label = "' '%up%variability", hjust = 0, col = ann_col, parse = TRUE) +
+  annotate("text", x = left, y = 2-gap, label = "' '%down%revenue", hjust = 0, col = ann_col, parse = TRUE)
+
+# p4
 
 pdf("portfolio/figs/stan-offset-break-anti-spaghetti.pdf", width = 10, height = 4)
-grid_arrange_shared_legend(p1, p2, ncol = 2, nrow = 1, position = "right")
+grid_arrange_shared_legend(p1, p3, ncol = 2, nrow = 1, position = "right")
 dev.off()
 
-# ifq
-
-ifq <- readr::read_csv("data/ifq_pfshy_year.csv")
-ifq <- ifq %>% filter(!is.na(year_catch_share))
-ifq
-
-ifq2 <- c("B61B", "B61B C61B", "B61B C61B S15B", "B61B S01", "B61B S03",
-  "B61B S03A", "B61B S03E", "B61B S15B", "C61B", "K91", "K91 T09",
-    "M7HB")
-
-res <- res %>% mutate(ifq = strategy %in% ifq2)
+ggsave("portfolio/figs/stan-offset-break-anti-spaghetti-specializing.pdf", plot = p4,
+  width = 6, height = 4.75)
