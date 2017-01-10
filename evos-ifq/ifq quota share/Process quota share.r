b83c119ad27ed8f2e2ce857551f0dcb8f356263b
OleShelton <ole.shelton@gmail.com>
2017-01-10 14:11:20 -0800

    removing old plots that don't matter.

--- evos-ifq/ifq quota share/Process quota share.r
+++ evos-ifq/ifq quota share/Process quota share.r
@@ -85,7 +85,7 @@ hal.dat.pre$YEAR <- as.numeric(as.character(hal.dat.pre$YEAR))
 hal.dat.pre<- hal.dat.pre %>% group_by(YEAR,AREA,CAT) %>% summarize(. , N= sum(N))
 
 # hal.dat.sum <- data.frame(hal.dat.sum)
-# hal.dat.sum$CAT <- as.character(hal.dat.sum$CAT)
+hal.dat.sum$CAT <- as.character(hal.dat.sum$CAT)
 hal.dat.sum <- rbind(hal.dat.sum,hal.dat.pre)
 
 hal.dat.sum$CAT[hal.dat.sum$CAT=="A"] <- "A"
@@ -125,60 +125,59 @@ part.3B<- ggplot(hal.dat.sum[hal.dat.sum$AREA == "3B" ,]) +
 START <- 1995
 STOP  <- 2015
 
-dist.2C <- ggplot()+
-  geom_boxplot(data=hal.dat.ind[hal.dat.ind$AREA == "2C" & hal.dat.ind$YEAR == START ,],
-              aes(y=PROP.Q,x=1,fill=CAT),outlier.shape=NA)+
-  geom_boxplot(data=hal.dat.ind[hal.dat.ind$AREA == "2C" & hal.dat.ind$YEAR == STOP ,],
-               aes(y=PROP.Q,x=2,fill=CAT),outlier.shape=NA)+
-  scale_y_continuous(trans="log10") +
-  scale_x_continuous(breaks=c(1,2),labels = c(START,STOP)) +
-  labs(x="Year",y="Prop. Quota") +
-  theme_bw() +
-  annotate("text", x = 2014, y = 2100, label = "Class")+
-  #theme(legend.position="none") +
-  #geom_vline(xintercept = 1995, linetype = "longdash") +
-  scale_fill_discrete(guide_legend(title = "2C"),values=COL)
-
-dist.3A <- ggplot()+
-  geom_boxplot(data=hal.dat.ind[hal.dat.ind$AREA == "3A" & hal.dat.ind$YEAR == START ,],
-               aes(y=PROP.Q,x=1,fill=CAT),outlier.shape=NA)+
-  geom_boxplot(data=hal.dat.ind[hal.dat.ind$AREA == "3A" & hal.dat.ind$YEAR == STOP ,],
-               aes(y=PROP.Q,x=2,fill=CAT),outlier.shape=NA)+
-  scale_y_continuous(trans="log10") +
-  scale_x_continuous(breaks=c(1,2),labels = c(START,STOP)) +
-  labs(x="Year",y="Prop. Quota") +
-  theme_bw() +
-  annotate("text", x = 2014, y = 2800, label = "3A")+
-    #theme(legend.position="none") +
-  scale_fill_discrete(guide_legend(title = "Class"))
-
-dist.3B <- ggplot()+
-  geom_boxplot(data=hal.dat.ind[hal.dat.ind$AREA == "3B" & hal.dat.ind$YEAR == START ,],
-               aes(y=PROP.Q,x=1,fill=CAT),outlier.shape=NA)+
-  geom_boxplot(data=hal.dat.ind[hal.dat.ind$AREA == "3B" & hal.dat.ind$YEAR == STOP ,],
-               aes(y=PROP.Q,x=2,fill=CAT),outlier.shape=NA)+
-  scale_y_continuous(trans="log10") +
-  scale_x_continuous(breaks=c(1,2),labels = c(START,STOP)) +
-  labs(x="Year",y="Prop. Quota") +
-  theme_bw() +
-  annotate("text", x = 2014, y = 2800, label = "3B")+
-  scale_fill_viridis(guide_legend(title = "Class"))
-  #theme(legend.position="none") +
-  #scale_fill_discrete()
-
-## Make Plot
-multiplot(part.2C,part.3A,part.3B,dist.2C,dist.3A,dist.3B,cols = 2)
-
-setwd("/Users/ole.shelton/GitHub/pfx-commercial/evos-ifq/ifq quota share/Plots")
-
-ylab <- ggplot()+labs(ylab="label")+theme_bw()
-
-
-# Layout= matrix(c(1,2,1,3,1,4),nrow=3,ncol=2,byrow=T)
-# QQ<- list(plot(axes=F,xlab="",ylab="",type="n"),part.2C,part.3A,part.3B)
-quartz(file="IFQ participation.pdf",type="pdf",dpi=600,height=7,width=5)
-  multiplot(part.2C,part.3A,part.3B,cols = 1)
-dev.off()
+# dist.2C <- ggplot()+
+#   geom_boxplot(data=hal.dat.ind[hal.dat.ind$AREA == "2C" & hal.dat.ind$YEAR == START ,],
+#               aes(y=PROP.Q,x=1,fill=CAT),outlier.shape=NA)+
+#   geom_boxplot(data=hal.dat.ind[hal.dat.ind$AREA == "2C" & hal.dat.ind$YEAR == STOP ,],
+#                aes(y=PROP.Q,x=2,fill=CAT),outlier.shape=NA)+
+#   scale_y_continuous(trans="log10") +
+#   scale_x_continuous(breaks=c(1,2),labels = c(START,STOP)) +
+#   labs(x="Year",y="Prop. Quota") +
+#   theme_bw() +
+#   #annotate("text", x = 2014, y = 2100, label = "Class")+
+#   #theme(legend.position="none") +
+#   #geom_vline(xintercept = 1995, linetype = "longdash") +
+#   scale_fill_discrete(guide_legend(title = "2C"),values=COL)
+#
+# dist.3A <- ggplot()+
+#   geom_boxplot(data=hal.dat.ind[hal.dat.ind$AREA == "3A" & hal.dat.ind$YEAR == START ,],
+#                aes(y=PROP.Q,x=1,fill=CAT),outlier.shape=NA)+
+#   geom_boxplot(data=hal.dat.ind[hal.dat.ind$AREA == "3A" & hal.dat.ind$YEAR == STOP ,],
+#                aes(y=PROP.Q,x=2,fill=CAT),outlier.shape=NA)+
+#   scale_y_continuous(trans="log10") +
+#   scale_x_continuous(breaks=c(1,2),labels = c(START,STOP)) +
+#   labs(x="Year",y="Prop. Quota") +
+#   theme_bw() +
+#   annotate("text", x = 2014, y = 2800, label = "3A")+
+#     #theme(legend.position="none") +
+#   scale_fill_discrete(guide_legend(title = "Class"))
+#
+# dist.3B <- ggplot()+
+#   geom_boxplot(data=hal.dat.ind[hal.dat.ind$AREA == "3B" & hal.dat.ind$YEAR == START ,],
+#                aes(y=PROP.Q,x=1,fill=CAT),outlier.shape=NA)+
+#   geom_boxplot(data=hal.dat.ind[hal.dat.ind$AREA == "3B" & hal.dat.ind$YEAR == STOP ,],
+#                aes(y=PROP.Q,x=2,fill=CAT),outlier.shape=NA)+
+#   scale_y_continuous(trans="log10") +
+#   scale_x_continuous(breaks=c(1,2),labels = c(START,STOP)) +
+#   labs(x="Year",y="Prop. Quota") +
+#   theme_bw() +
+#   annotate("text", x = 2014, y = 2800, label = "3B")+
+#   scale_fill_viridis(guide_legend(title = "Class"))
+#   #theme(legend.position="none") +
+#   #scale_fill_discrete()
+#
+# ## Make Plot
+# multiplot(part.2C,part.3A,part.3B,dist.2C,dist.3A,dist.3B,cols = 2)
+#
+# setwd("/Users/ole.shelton/GitHub/pfx-commercial/evos-ifq/ifq quota share/Plots")
+#
+# ylab <- ggplot()+labs(ylab="label")+theme_bw()
+#
+# # Layout= matrix(c(1,2,1,3,1,4),nrow=3,ncol=2,byrow=T)
+# # QQ<- list(plot(axes=F,xlab="",ylab="",type="n"),part.2C,part.3A,part.3B)
+# quartz(file="IFQ participation.pdf",type="pdf",dpi=600,height=7,width=5)
+#   multiplot(part.2C,part.3A,part.3B,cols = 1)
+# dev.off()
 
 
 
