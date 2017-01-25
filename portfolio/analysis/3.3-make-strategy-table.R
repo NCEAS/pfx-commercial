256d1089469c6a7853ca274ce6f764a279df9c05
Sean Anderson <sean@seananderson.ca>
2017-01-24 22:16:50 -0800

    Incorporate comments

--- portfolio/analysis/3.3-make-strategy-table.R
+++ portfolio/analysis/3.3-make-strategy-table.R
@@ -4,16 +4,27 @@ d <- readr::read_csv("data/strategies-labels.csv")
 load("portfolio/data-generated/diff-dat-stan.rda")
 d <- d %>% mutate(modeled = ifelse(strategy %in% md$strategy, "Yes", "No"))
 
-d <- select(d, str_label, description, modeled) %>%
+# spp <- group_by(dat, strategy) %>%
+  # summarise(
+    # spp_l = round(quantile(specDiv, prob = 0.25), 1),
+    # spp_h = round(quantile(specDiv, prob = 0.75), 1))
+
+# d <- inner_join(d, spp)
+d <- inner_join(d, select(md, strategy, strategy_mean_div))
+
+d <- select(d, str_label, description, strategy_mean_div, modeled) %>%
   rename(Label = str_label, Description = description,
-    `Over 100 permit holders` = modeled) %>%
-  arrange(Label)
+    `Over 100 permit holders` = modeled,
+    `Mean spp. diversity` = strategy_mean_div) %>%
+  arrange(`Mean spp. diversity`) %>%
+  mutate(`Mean spp. diversity` = round(`Mean spp. diversity`, 1))
+
 d <- d %>% filter(`Over 100 permit holders` == "Yes") %>%
   select(-`Over 100 permit holders`)
 
 xtable::print.xtable(
   xtable::xtable(d,
-    caption = ""),
+    caption = "", digits = 1),
   include.rownames = FALSE,
   file = "portfolio/figs/strategy-table.tex",
   booktabs = TRUE,
