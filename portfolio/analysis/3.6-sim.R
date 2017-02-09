e20ee74d14d7d32a33490a3694748dfde39491c0
Sean Anderson <sean@seananderson.ca>
2017-02-09 14:56:10 -0800

    Adjust simulation test

--- portfolio/analysis/3.6-sim.R
+++ portfolio/analysis/3.6-sim.R
@@ -97,38 +97,9 @@ standat <- list(
   mean_day_str = xh1
 )
 
-beta_init <- function() rnorm(standat$J)
-sigma_init <- function() rnorm(standat$K)
-dev_str_init <- function() rnorm(standat$n_strategy, 0, 0.05)
-dev_yr_init <- function() rnorm(standat$n_str_yr, 0, 0.05)
-tau_init <- function() runif(1, 0.05, 0.1)
-init_fun <- function() {
-  list(
-    b0 = beta_init()[1],
-    b0_strategy = dev_str_init(),
-    b0_str_yr = dev_yr_init(),
-    b0_strategy_tau = tau_init(),
-    b0_str_yr_tau = tau_init(),
-    b_j = beta_init(),
-    h1 = beta_init()[1],
-    h2 = beta_init()[1],
-    b1_strategy = dev_str_init(),
-    b1_strategy_tau = tau_init(),
-    b2_strategy = dev_str_init(),
-    b2_strategy_tau = tau_init(),
-    g0 = sigma_init()[1],
-    g0_strategy = dev_str_init(),
-    g0_strategy_tau = tau_init(),
-    g_k = sigma_init(),
-    g1_strategy = dev_str_init(),
-    g1_strategy_tau = tau_init(),
-    g2_strategy = dev_str_init(),
-    g2_strategy_tau = tau_init())
-}
-
 msim <- stan("portfolio/analysis/portfolio-offset-sim.stan",
-  data = standat, iter = 400, chains = 4,
-  pars = c("mu", "sigma"), include = FALSE, init = init_fun)
+  data = standat, iter = 500, chains = 2,
+  pars = c("mu", "sigma"), include = FALSE)
 save(msim, file = "portfolio/data-generated/nsim.rda")
 load("portfolio/data-generated/nsim.rda")
 
